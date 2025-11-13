5b7b0afeb73373d7f8e8fbf3e5b6d35e5b684a70
sharmila1369 <sharmilambikavenna@gmail.com>
2025-11-13 14:31:45 +0530

    Added render.yaml for deployment

--- render.yaml.py
+++
@@ -1,8 +0,0 @@
-services:
-  - type: web
-    name: telco-churn-prediction
-    runtime: python
-    buildCommand: "pip install -r requirements.txt"
-    startCommand: "gunicorn app:app"
-    envVars:
-      PYTHON_VERSION: 3.10
